a4f8e391867efa5068ddd7d4ecbe0d618e62bbe1
GIOgiorgadze <ggiorgadze579@gmail.com>
2023-06-12 10:46:30 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import logo1 from './images/scooby-do.jpg';
+import logo2 from './images/shaggy.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="section1">
+            <h1>Scooby Doo's adventures</h1>
+            <img src={logo1} alt='scooby-doo'></img>
+            <ul className='list'>
+              <li>Opened 120 cases</li>
+              <li>Eate vast of hamburgers</li>
+              <li>catch 25 monsters</li>
+            </ul>
+        </div>  
+        <div className="section2">
+            <h1>Shegy's advetures</h1>
+            <img src={logo2} alt='shaggy' ></img>
+            <ul className='list l2'>
+              <li>Opened 110 cases</li>
+              <li>Eate vast of hamburgers</li>
+              <li>catch 18 monsters</li>
+            </ul>
+        </div>  
       </header>
     </div>
   );
